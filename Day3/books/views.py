718d29406cd94881125cca86271f17c822b6191e
Mohd Asad Ansari <asadansari905811@gmail.com>
2025-10-02 14:01:34 +0530

    Serialization and class based view

---
+++ Day3/books/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .models import Author, Book
+from .serializers import AuthorSerializer, BookSerializer
+
+# Book Views
+class BookListCreateView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+# Author Views
+class AuthorListCreateView(generics.ListCreateAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+class AuthorDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer

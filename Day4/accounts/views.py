3ae1d169285072e89d3c0d3300ad500f1b881f05
Mohd Asad Ansari <asadansari905811@gmail.com>
2025-10-08 22:20:05 +0530

    inbuilt auth user

---
+++ Day4/accounts/views.py
@@ -0,0 +1,12 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .serializers import UserSerializer
+
+class ProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
